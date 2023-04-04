3261ddccc32485ed6ef28eb7155a54fbdbd8763c
David Bakare <bakaredavid007@gmail.com>
2023-04-04 02:40:48 +0100

    measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+measure_time function module
+'''
+
+
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''
+    measure_time function
+    '''
+
+    s = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elasped = time.perf_counter() - s
+    return (elasped/n)
