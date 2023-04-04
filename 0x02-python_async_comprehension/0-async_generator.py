196301086701e8d60ea2feedbb6a6173fbdf9d92
David Bakare <bakaredavid007@gmail.com>
2023-04-04 12:06:02 +0100

    intial commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+async generator function module
+'''
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    '''
+    async_generator function
+    '''
+
+    for i in range(10):
+        rand = random.uniform(0, 10)
+        yield rand
+    await asyncio.sleep(1)
