8d349cf77484b72dc35d97021b8be034e239532d
David Bakare <bakaredavid007@gmail.com>
2023-04-04 03:04:10 +0100

    changed time.perf_counter to time.time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,7 +14,7 @@ def measure_time(n: int, max_delay: int) -> float:
     measure_time function
     '''
 
-    s = time.perf_counter()
+    s = time.time()
     asyncio.run(wait_n(n, max_delay))
-    elasped = time.perf_counter() - s
+    elasped = time.time() - s
     return (elasped/n)
